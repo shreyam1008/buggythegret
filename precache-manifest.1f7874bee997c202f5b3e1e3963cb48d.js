e2826b31d1d13b684d0e7c064e54f876b67246d8
Shreyam Adhikari <shreyam1008@gmail.com>
2021-04-11 00:27:05 +0545

    Updates

---
+++ precache-manifest.1f7874bee997c202f5b3e1e3963cb48d.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1c59c8b69ed23269ab8978f850cd709",
+    "url": "/buggythegret/index.html"
+  },
+  {
+    "revision": "b4dcccef3c15fefb5363",
+    "url": "/buggythegret/static/css/main.a41be33c.chunk.css"
+  },
+  {
+    "revision": "578810b61ede407af254",
+    "url": "/buggythegret/static/js/2.c74e1709.chunk.js"
+  },
+  {
+    "revision": "bfbf98b39da508f522b8cab91a8e0702",
+    "url": "/buggythegret/static/js/2.c74e1709.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4dcccef3c15fefb5363",
+    "url": "/buggythegret/static/js/main.1bb58ef2.chunk.js"
+  },
+  {
+    "revision": "146c4c440d326f1c1009",
+    "url": "/buggythegret/static/js/runtime-main.997dba04.js"
+  },
+  {
+    "revision": "7c8e5f50725885d51b72b45164274e41",
+    "url": "/buggythegret/static/media/avatar.7c8e5f50.png"
+  },
+  {
+    "revision": "2dd213aa54b22c3bca0c62f570d1512a",
+    "url": "/buggythegret/static/media/coffee-apple.2dd213aa.jpg"
+  },
+  {
+    "revision": "54fd423489a1937caf8950d12c8a8526",
+    "url": "/buggythegret/static/media/html-css-javascript-lg.54fd4234.jpg"
+  },
+  {
+    "revision": "2c7701aae07adecdb0a7e336dff08def",
+    "url": "/buggythegret/static/media/html-css-javascript.2c7701aa.jpg"
+  },
+  {
+    "revision": "730958a228d26dee011c5aa0aa3c3db9",
+    "url": "/buggythegret/static/media/javascript-fullstack.730958a2.jpg"
+  },
+  {
+    "revision": "90616a5dc9acdcce7158addc89620052",
+    "url": "/buggythegret/static/media/mern-stack.90616a5d.jpg"
+  },
+  {
+    "revision": "a8b012a0d7afd08ee07251e1f67294ff",
+    "url": "/buggythegret/static/media/react-redux.a8b012a0.jpg"
+  },
+  {
+    "revision": "f370c1e0adb80194dd3ea10e468bc69a",
+    "url": "/buggythegret/static/media/react.f370c1e0.png"
+  }
+]);
